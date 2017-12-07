4ffd45cd36f8e908cb82336d49ba61b14f0f0eb7
zsarlenealyzagarcia <alyzang1118@gmail.com>
2017-12-07 15:38:17 +0800

    updated table of doctor

--- clinic.sql
+++ clinic.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2017 at 06:27 AM
+-- Generation Time: Dec 07, 2017 at 07:37 AM
 -- Server version: 5.7.9
 -- PHP Version: 5.6.16
 
@@ -44,16 +44,21 @@ CREATE TABLE IF NOT EXISTS `doctor` (
 --
 
 INSERT INTO `doctor` (`employee_id`, `last_name`, `first_name`, `specialty`, `start_time`, `end_time`, `day_available`, `room_number`) VALUES
-(1, 'Dalton', 'Waylon', 'Oncologist', NULL, NULL, NULL, NULL),
-(2, 'Cruz', 'Marcuz', 'Peditrician', NULL, NULL, NULL, NULL),
-(3, 'Randolf', 'Eddie', 'Orthopedics', NULL, NULL, NULL, NULL),
-(4, 'Hartman', 'Hadasa', 'Psychiatrist', NULL, NULL, NULL, NULL),
-(5, 'Henderson', 'Justine', 'Surgeon', NULL, NULL, NULL, NULL),
-(6, 'Cobb', 'Thalia', 'Pediatrician', NULL, NULL, NULL, NULL),
-(7, 'Walker', 'Angela', 'Dermatologist', NULL, NULL, NULL, NULL),
-(8, 'Shaffer', 'Joanna', 'Epidemiologist', NULL, NULL, NULL, NULL),
-(9, 'Lang', 'Abdulah', 'Surgeon', NULL, NULL, NULL, NULL),
-(10, 'Little', 'Matthias', 'Cardiologist', NULL, NULL, NULL, NULL);
+(1, 'Dalton', 'Waylon', 'Oncologist', '09:00:00', '11:00:00', 'MWF', 'R1'),
+(2, 'Cruz', 'Marcuz', 'Peditrician', '10:00:00', '12:00:00', 'WTTH', 'R2'),
+(3, 'Randolf', 'Eddie', 'Orthopedics', '14:00:00', '16:00:00', 'TWTH', 'R3'),
+(4, 'Hartman', 'Hadasa', 'Psychiatrist', '15:00:00', '17:00:00', 'TTHF', 'R4'),
+(5, 'Henderson', 'Justine', 'Surgeon', '9:00:00', '12:00:00', 'MTW', 'R5'),
+(6, 'Cobb', 'Thalia', 'Pediatrician', '14:00:00', '17:00:00', 'MTW', 'R6'),
+(7, 'Walker', 'Angela', 'Dermatologist', '15:00:00', '17:00:00', 'TTHFS', 'R7'),
+(8, 'Shaffer', 'Joanna', 'Epidemiologist', '10:00:00', '12:00:00', 'WTHFS', 'R8'),
+(9, 'Lang', 'Abdulah', 'Opthalmologist', '9:00:00', '11:00:00', 'MWF', 'R09'),
+(10, 'Little', 'Matthias', 'Cardiologist', '14:00:00', '16:00:00', 'MWTH', 'R10'),
+(11, 'Brueggeman', 'Katherine', 'Gastroenterology', '9:00:00', '11:00:00', NULL, 'R01'),
+(12, 'Beavers', 'Beatrice', 'Radiology', '10:00:00', '12:00:00', NULL, 'R02'),
+(13, 'Pillai', 'Leann', 'Pathology', '14:00:00', '16:00:00', NULL, 'R03'),
+(14, 'Fraire', 'Prudence', 'Physician', '14:00:00', '17:00:00', NULL, 'R04'),
+(15, 'Pavone', 'Darron', 'Pulmonologist', '9:00:00', '12:00:00', NULL, 'R05');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
